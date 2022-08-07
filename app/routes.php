31ab4f92313c98fda222934b9f2dc614d3fa4841
AslanAV <autlevaslan@yandex.ru>
2022-08-08 01:34:12 +0300

    add group for balance routes.php

--- app/routes.php
+++ app/routes.php
@@ -21,11 +21,13 @@ return function (App $app) {
         return $response;
     });
 
-//    $app->group('/users', function (Group $group) {
-//        $group->get('', ListUsersAction::class);
-//        $group->get('/{id}', ViewUserAction::class);
-//    });
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
 
-    $app->post('/{user1}/{operation}/{count}', OperationWithBalanceAction::class);
-    $app->get('/{user1}/show', ShowBalanceAction::class);
+    $app->group('/{user1}', function (Group $group) {
+        $group->post('/{operation}/{count}', OperationWithBalanceAction::class);
+        $group->get('/show', ShowBalanceAction::class);
+    });
 };
